1639cde1d5330287a1399b2fc97153eb4d17cb63
Kamil Kowalewski <22kamil.kow@gmail.com>
2021-06-13 21:00:58 +0200

    Final project

--- test.s
+++ test.s
@@ -1,12 +1,15 @@
-a_len = 200
-a_str_len = 200
+age_len = 200
+age_str_len = 200
 .data
-a: .int 5
-a_str: .ascii "5"
-a_str_len = .- a_str
+text1: .ascii "How old are you?\n"
+text1_len = .- text1
+text2: .ascii "You are "
+text2_len = .- text2
+text3: .ascii "\n"
+text3_len = .- text3
 .bss
-a: .space a_len
-a_str: .space a_str_len
+age: .space age_len
+age_str: .space age_str_len
 .text
 .global _start
 _start:
@@ -15,12 +18,36 @@ mov %eax, %ebx
 mov $1, %eax
 int $0x80
 
-.global main
-main:
+.global greet
+greet:
 mov $4, %eax
 mov $1, %ebx
-mov $a_str , %ecx
-mov $a_str_len, %edx
+mov $text1, %ecx
+mov $text1_len, %edx
 int $0x80
+mov $3, %eax
+mov $0, %ebx
+mov $age_str, %ecx
+mov $age_str_len, %edx
+int $0x80
+mov $4, %eax
+mov $1, %ebx
+mov $text2, %ecx
+mov $text2_len, %edx
+int $0x80
+mov $4, %eax
+mov $1, %ebx
+mov $age_str , %ecx
+mov $age_str_len, %edx
+int $0x80
+mov $4, %eax
+mov $1, %ebx
+mov $text3, %ecx
+mov $text3_len, %edx
+int $0x80
+ret
+.global main
+main:
+call greet
 mov $0, %eax
 ret
